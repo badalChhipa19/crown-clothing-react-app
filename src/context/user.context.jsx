18aa646d5de265d794b580efc79c09ae0bd3b4da
Badal <badal@NETURON17>
2023-07-05 01:01:53 +0530

    useState replaced by useReducer in contexts

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   OnAuthStateChangedListner,
   CreateUserDocumentFromAuth,
@@ -9,8 +9,37 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+const USER_ACTION_TYPE = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReduser = (state, action) => {
+  const { type, paylod } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPE.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: paylod,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in useReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [{ currentUser }, dispatch] = useReducer(userReduser, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: "SET_CURRENT_USER", paylod: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
