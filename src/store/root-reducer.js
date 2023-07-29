17718d02ad3e25651c51084e1495fa7e2dd0fcf7
Badal <badal@NETURON17>
2023-07-30 01:20:50 +0530

    Redux toolkit install, and store and root reducer updated accordingly

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,6 @@
-import { userReducer } from "./user/user.reducer";
+import { combineReducers } from "@reduxjs/toolkit";
 
-import { combineReducers } from "redux";
+import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/categories.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,42 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
+// import { compose, createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import logger from "redux-logger";
+// import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
-const middleware = [
-  process.env.NODE_ENV !== "production" && logger,
-  thunk,
-].filter(Boolean);
-// const middleware = [logger];
+// const middleware = [
+//   process.env.NODE_ENV !== "production" && logger,
+//   thunk,
+// ].filter(Boolean);
+// // const middleware = [logger];
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["cart"],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleware));
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleware));
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
-export const persistor = persistStore(store);
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
+
+// export const persistor = persistStore(store);
